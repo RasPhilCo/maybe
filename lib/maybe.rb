4e17e50faa6e75ec532dfc3939dcc1bf9dbad87b
RasPhilCo <RasPhilCo@Versus.local>
2016-08-10 17:55:03 -0700

    Init commit, v0.1.0

---
+++ lib/maybe.rb
@@ -0,0 +1,29 @@
+require "maybe/version"
+
+module Maybe
+  class NullObject
+    def method_missing(*args, &block)
+      self
+    end
+
+    # conversions
+    def to_a; []; end
+    def to_s; ""; end
+    def to_f; 0.0; end
+    def to_i; 0; end
+
+    # rails helpers
+    def present?; false; end
+    def empty?; true; end
+
+    # misc
+    def tap; self; end
+  end
+end
+
+def Maybe(value)
+  case value
+  when nil then Maybe::NullObject.new
+  else value
+  end
+end
---
+++ test/maybe_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class MaybeTest < Minitest::Test
+  def setup
+    @maybe = Maybe(nil)
+  end
+
+  def test_that_it_has_a_version_number
+    refute_nil ::Maybe::VERSION
+  end
+
+  def test_missing_method_returns_itself
+    assert @maybe, @maybe.undefined_method
+  end
+
+  def test_conversions
+    assert [], @maybe.to_a
+    assert "", @maybe.to_s
+    assert 0.0, @maybe.to_f
+    assert 0, @maybe.to_i
+  end
+
+  def test_rails_helpers
+    assert_same false, @maybe.present?
+    assert_same true, @maybe.empty?
+  end
+
+  def test_misc
+    assert_silent{ @maybe.tap { raise } }
+  end
+end
+
+class MaybeNotNilTest < Minitest::Test
+  def setup
+    @maybe = Maybe(Object.new)
+  end
+
+  def test_constructor_returns_not_nil
+    refute_nil @maybe
+  end
+end
